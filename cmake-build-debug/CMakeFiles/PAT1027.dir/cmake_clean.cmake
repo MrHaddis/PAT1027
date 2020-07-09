dea1e56253749a980d1a82a1279cdd867775779b
duanyuxiang <duanyx@belink.com>
2020-07-09 16:19:53 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1027.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1027.dir/main.c.obj"
+  "PAT1027.exe"
+  "PAT1027.exe.manifest"
+  "PAT1027.pdb"
+  "libPAT1027.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1027.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PAT1027.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PAT1027.
+# This may be replaced when dependencies are built.
